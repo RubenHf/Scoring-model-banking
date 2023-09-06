5c28d709c1857d1887d0e82e44a5ffeeb4b482c6
Rubenhf <blqckaus@gmail.com>
2023-09-06 13:00:17 +0200

    test app

--- banking-dash-app.py
+++ banking-dash-app.py
@@ -1,8 +1,6 @@
 # app.py
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash import Dash, html, dash_table, dcc, callback, Output, Input, State
 
 app = dash.Dash(__name__)
 
